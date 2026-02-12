13cd455dd6a1cac73f28f4f1e1220c45c4004487
Richard Irons <richard.irons@neo4j.com>
2026-02-12 14:35:55 +0000

    Temporary commit

--- Neo4j.Driver/Neo4j.Driver/Public/Types/Vector.cs
+++ Neo4j.Driver/Neo4j.Driver/Public/Types/Vector.cs
@@ -80,12 +80,9 @@ public abstract class Vector : IValue, IVector, IEquatable<IVector>
     /// <exception cref="NotSupportedException">Thrown if <typeparamref name="T"/> is not a supported type.</exception>
     public static Vector<T> Create<T>(T[] values, byte[] originalByteStream = null) where T : struct
     {
-        if (!IsSupported(typeof(T)))
-        {
-            throw new NotSupportedException($"Type {typeof(T).Name} is not supported for Vector.");
-        }
-
-        return new Vector<T>(values, originalByteStream);
+        return !IsSupported(typeof(T))
+            ? throw new NotSupportedException($"Type {typeof(T).Name} is not supported for Vector.")
+            : new Vector<T>(values, originalByteStream);
     }
 
     private static readonly MethodInfo CreateMethodInfo = typeof(Vector).GetMethod(nameof(Create));
